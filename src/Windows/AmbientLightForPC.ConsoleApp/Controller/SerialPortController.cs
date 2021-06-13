f00add99338cb861b84b3028e4850f6829342395
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-06-13 10:31:05 +0100

    add serial port controller

---
+++ src/Windows/AmbientLightForPC.ConsoleApp/Controller/SerialPortController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+
+namespace AmbientLightForPC.Controller
+{
+    public class SerialPortController : SerialPort
+    {
+        private const int BUF_SIZE = 128;
+        
+        public event StringDataHandler StringDataReceived;
+
+        public delegate void StringDataHandler(string value);
+
+        public SerialPortController(string portName,
+            int baudRate,
+            Parity parity = Parity.None,
+            int dataBits = 8,
+            StopBits stopBits = StopBits.One) : base(portName, baudRate, parity, dataBits, stopBits)
+        {
+            DataReceived += (_, _) =>
+            {
+                if (StringDataReceived == null)
+                    return;
+
+                byte[] inputBuf = new byte[BUF_SIZE];
+
+                try
+                {
+                    Read(inputBuf, 0, BytesToRead);
+                    StringDataReceived(Encoding.ASCII.GetString(inputBuf));
+                }
+                catch (TimeoutException ex)
+                {
+                    StringDataReceived("");
+                }
+            };
+        }
+    }
+}
