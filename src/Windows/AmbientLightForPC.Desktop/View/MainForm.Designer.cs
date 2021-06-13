9aa84e5771a4e6185ad56d150a1f9f6938a6dc24
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-06-13 11:29:12 +0100

    add GUI

---
+++ src/Windows/AmbientLightForPC.Desktop/View/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace AmbientLightForPC.Desktop.View
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tkbBrightness = new System.Windows.Forms.TrackBar();
+            this.cmbBrightnessController = new System.Windows.Forms.ComboBox();
+            this.numBrightness = new System.Windows.Forms.NumericUpDown();
+            this.btnApply = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tkbBrightness)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBrightness)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tkbBrightness
+            // 
+            this.tkbBrightness.Location = new System.Drawing.Point(12, 122);
+            this.tkbBrightness.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tkbBrightness.Maximum = 255;
+            this.tkbBrightness.Name = "tkbBrightness";
+            this.tkbBrightness.Size = new System.Drawing.Size(328, 56);
+            this.tkbBrightness.TabIndex = 0;
+            this.tkbBrightness.Scroll += new System.EventHandler(this.tkbBrightness_Scroll);
+            // 
+            // cmbBrightnessController
+            // 
+            this.cmbBrightnessController.FormattingEnabled = true;
+            this.cmbBrightnessController.Location = new System.Drawing.Point(12, 12);
+            this.cmbBrightnessController.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cmbBrightnessController.Name = "cmbBrightnessController";
+            this.cmbBrightnessController.Size = new System.Drawing.Size(283, 28);
+            this.cmbBrightnessController.TabIndex = 1;
+            // 
+            // numBrightness
+            // 
+            this.numBrightness.Location = new System.Drawing.Point(371, 134);
+            this.numBrightness.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.numBrightness.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numBrightness.Name = "numBrightness";
+            this.numBrightness.Size = new System.Drawing.Size(150, 27);
+            this.numBrightness.TabIndex = 2;
+            this.numBrightness.ValueChanged += new System.EventHandler(this.numBrightness_ValueChanged);
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(479, 266);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(94, 29);
+            this.btnApply.TabIndex = 3;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.numBrightness);
+            this.Controls.Add(this.cmbBrightnessController);
+            this.Controls.Add(this.tkbBrightness);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tkbBrightness)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBrightness)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tkbBrightness;
+        private System.Windows.Forms.ComboBox cmbBrightnessController;
+        private System.Windows.Forms.NumericUpDown numBrightness;
+        private System.Windows.Forms.Button btnApply;
+    }
+}
+
---
+++ src/Windows/AmbientLightForPC.Desktop/View/MainForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows.Forms;
+using AmbientLightForPC.Library;
+using AmbientLightForPC.Library.Controller;
+using AmbientLightForPC.Plugin;
+
+namespace AmbientLightForPC.Desktop.View
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void tkbBrightness_Scroll(object sender, EventArgs e)
+        {
+            numBrightness.Value = tkbBrightness.Value;
+        }
+
+        bool hasShowedWarning = false;
+
+        private void numBrightness_ValueChanged(object sender, EventArgs e)
+        {
+            tkbBrightness.Value = (int) numBrightness.Value;
+
+            if (tkbBrightness.Value <= 100 || hasShowedWarning)
+                return;
+
+            hasShowedWarning = true;
+            if (MessageBox.Show(
+                "Over 100 value may cause display unproperly! Are you sure you want to do this?",
+                "Warning",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.No)
+            {
+                tkbBrightness.Value = tkbBrightness.Value = 100;
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            cmbBrightnessController.Items.Add(Factory.CreateController<DefaultBrightnessController>());
+            cmbBrightnessController.Items.Add(Factory.CreateController<GammaBrightnessController>());
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            object selected = cmbBrightnessController.SelectedItem;
+            if (selected == null)
+                return;
+            BrightnessControllerBase bcb = (BrightnessControllerBase) selected;
+            bcb.TryControlBrightness((byte) tkbBrightness.Value);
+        }
+    }
+}
