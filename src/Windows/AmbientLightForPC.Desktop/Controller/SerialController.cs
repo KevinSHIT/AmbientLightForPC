c834ee31373bcf15a7c7132013feb7f3f3e4872d
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-06-14 19:55:35 +0100

    add serial controller

---
+++ src/Windows/AmbientLightForPC.Desktop/Controller/SerialController.cs
@@ -0,0 +1,40 @@
+﻿using AmbientLightForPC.Library.Controller;
+
+namespace AmbientLightForPC.Desktop.Controller
+{
+    public class SerialController
+    {
+        public event ByteDataHandler OnByteDataReceived;
+        public delegate void ByteDataHandler(byte value);
+        public event StringDataHandler OnLogDataReceived;
+        public delegate void StringDataHandler(string value);
+        
+        public SerialController(SerialPortController src)
+        {
+            SerialPortController = src;
+            src.OnStringDataReceived += (s) =>
+            {
+                if (s.StartsWith("E") || s.StartsWith("I") || s.StartsWith("W"))
+                {
+                    Log(s);
+                    return;
+                }
+
+                if (!byte.TryParse(s, out byte v))
+                {
+                    Log(s);
+                    return;
+                }
+
+                OnByteDataReceived?.Invoke(v);
+            };
+        }
+
+        public SerialPortController SerialPortController { get; }
+
+        private void Log(string msg)
+        {
+            OnLogDataReceived?.Invoke(msg);
+        }
+    }
+}
