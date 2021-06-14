28b8393a6390032ae9485df327cc649ab732c5ab
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-06-14 13:24:39 +0100

    add BrightnessController"

--- src/Windows/AmbientLightForPC.Desktop/Controller/BrightnessController.cs
+++ src/Windows/AmbientLightForPC.Desktop/Controller/BrightnessController.cs
@@ -1,7 +1,41 @@
-﻿namespace AmbientLightForPC.Desktop.Controller
+﻿using System.Collections.Generic;
+using System.Linq;
+using AmbientLightForPC.Desktop.Model;
+
+namespace AmbientLightForPC.Desktop.Controller
 {
     public class BrightnessController
     {
-        
+        private readonly DisplayModel _displayModel;
+        private List<int> _lux = new List<int>();
+
+        public BrightnessController(DisplayModel dm)
+        {
+            _displayModel = dm;
+            foreach (var kvp in _displayModel.Lux)
+            {
+                _lux.Add(kvp.Key);
+            }
+
+            _lux.Sort();
+        }
+
+        public ControlInfoModel GetControlInfo(int lux)
+        {
+            if (_displayModel == null || _lux == null)
+                return null;
+
+            for (int i = 0; i < _lux.Count; ++i)
+            {
+                if (_lux[i] > lux)
+                {
+                    return i > 0
+                        ? _displayModel.Lux[_lux[i - 1]]
+                        : _displayModel.Lux[0];
+                }
+            }
+
+            return _displayModel.Lux.Last().Value;
+        }
     }
 }
