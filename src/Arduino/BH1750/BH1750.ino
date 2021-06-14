f42e6cf0c4516410c6e46544585a73bf63242897
KevinZonda <33132228+KevinZonda@users.noreply.github.com>
2021-06-14 20:51:07 +0100

    support BH1750

---
+++ src/Arduino/BH1750/BH1750.ino
@@ -0,0 +1,41 @@
+
+/*
+ * Project: AmbientLightForPC
+ * Author: KevinZonda
+ * Date: 13/06/2021
+ * Link: https://github.com/KevinZonda/AmbientLightForPC
+ * Description: This programme uses library https://github.com/claws/BH1750
+ * 
+ * 3.3V         VCC              Power
+ * GND          GND              Ground
+ * D3           SDA              I2C Data
+ * D4           SCL              I2C Clock
+ */
+#include <Wire.h>
+#include <BH1750.h>
+
+BH1750 bh;
+
+void setup()
+{
+
+    Serial.begin(9600);
+
+    // Initialise I2C
+    Wire.begin(D3, D4);
+    bh.begin();
+
+    Serial.println();
+    Serial.println(F("I: -------------------------------------"));
+    Serial.println(F("I:   AmbientLightForPC - BH1750         "));
+    Serial.println(F("I:   https://github.com/KevinZonda      "));
+    Serial.println(F("I: -------------------------------------"));
+}
+
+void loop()
+{
+
+    float v = bh.readLightLevel();
+    Serial.println(v);
+    delay(250);
+}
